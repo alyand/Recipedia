949e73faa954ade20f6567c18f0b81e3eb707706
evasbr <evasbr19@gmail.com>
2025-06-25 10:11:54 +0700

    fix : docker compose env, api-gateway url. feat : rate limiter

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -2,15 +2,38 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 const PORT = process.env.PORT;
+const { rateLimit } = require("express-rate-limit");
+
+const userLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const recipeLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 200,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const reviewLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 200,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 /// == USER SERVICE ==
 app.use(
   "/user",
+  userLimiter,
   createProxyMiddleware({
-    target: "http://user-service:8001",
+    target: "http://user-service:8001/api",
     changeOrigin: true,
     pathRewrite: {
-      "^/user-service": "",
+      "^/user-service/api": "",
     },
   })
 );
@@ -18,6 +41,7 @@ app.use(
 // == RECIPE SERVICE ==
 app.use(
   "/recipe",
+  recipeLimiter,
   createProxyMiddleware({
     target: "http://recipe-service:8002",
     changeOrigin: true,
@@ -30,6 +54,7 @@ app.use(
 // == REVIEW SERVICE ==
 app.use(
   "/review",
+  reviewLimiter,
   createProxyMiddleware({
     target: "http://review-service:8003",
     changeOrigin: true,
--- recipe-service/src/index.js
+++ recipe-service/src/index.js
@@ -9,8 +9,8 @@ const MONGODB_URI = process.env.MONGO_URL;
 const PORT = process.env.PORT | "8002";
 
 app.use(express.json());
-app.use("/", require("./routes/externalRoutes"));
-app.use("/internal/recipes", require("./routes/internalRoutes"));
+app.use("/api", require("./routes/externalRoutes"));
+app.use("/internal", require("./routes/internalRoutes"));
 
 mongoose.connect(MONGODB_URI).then(() => {
   console.log("MongoDB connected");
